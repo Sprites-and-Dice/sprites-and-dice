9659b8b392266e178b65d84db05935207f98c45f
juan0tron <juandglover@gmail.com>
2019-12-01 00:23:49 -0800

    Add Game/Podcast choosers to pages, made progress on page template
    - Youtube/Podcast embeds
    - Social Media links
    - Update Wagtail to 2.7, Wagtailmedia to latest github commit

--- page/models.py
+++ page/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from modelcluster.contrib.taggit import ClusterTaggableManager
 from modelcluster.fields import ParentalKey
 
+from spritesanddice.stream_blocks import stream_blocks
+
 from taggit.models import TaggedItemBase
 
 from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, InlinePanel, StreamFieldPanel
@@ -12,13 +14,6 @@ from wagtail.core.models import Page, Orderable
 from wagtail.images.blocks import ImageChooserBlock
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
-
-sharedStreamFields = [
-	('heading', blocks.CharBlock(classname="full title")),
-	('paragraph', blocks.RichTextBlock()),
-	('image', ImageChooserBlock()),
-]
-
 
 class PageTag(TaggedItemBase):
 	content_object = ParentalKey(
@@ -33,13 +28,13 @@ class BasePage(Page):
 	promote_panels = Page.promote_panels + []
 	search_fields  = Page.search_fields  + []
 
-	# Return the first available "Paragraph" block
+	# Return the first available Rich Text block
 	def get_content_preview(self):
 		if(self.content):
 			for block in self.content:
-				if block.block.name == 'paragraph':
+				if block.block.name == 'Rich_Text':
 					return block.value
-			return ''
+		return ''
 
 	class Meta:
 		abstract = True
@@ -54,9 +49,13 @@ class BlogPage(BasePage):
 		related_name='+'
 	)
 
-	subtitle     = models.CharField(max_length=250, blank=True)
-	content      = StreamField(sharedStreamFields, blank=True)
-	tags         = ClusterTaggableManager(through=PageTag, blank=True)
+	subtitle = models.CharField(max_length=250, blank=True)
+	content  = StreamField(stream_blocks, blank=True)
+	tags     = ClusterTaggableManager(through=PageTag, blank=True)
+
+	# TODO: Determine how disqus identifiers for old pages will be stored
+	def disqus_identifier(self):
+		return self.__str__()
 
 	content_panels = BasePage.content_panels + [
 		FieldPanel('subtitle'),
@@ -64,5 +63,7 @@ class BlogPage(BasePage):
 		StreamFieldPanel('content'),
 		FieldPanel('tags'),
 	]
+
 	promote_panels = BasePage.promote_panels + []
+
 	search_fields  = BasePage.search_fields  + []
