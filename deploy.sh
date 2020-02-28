a1743c37a65e739771e968c1bb1cf81e07581272
juan0tron <juandglover@gmail.com>
2020-02-27 18:31:35 -0800

    Fix deploy script, don't limit parents of HomePage

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,14 @@ echo "Enabling Virtualenv..."
 source /home/sprites/sprites/bin/activate
 
 echo "Downloading updates from Git..."
-sudo git checkout .
-sudo git pull
+git checkout .
+git pull
 
 echo "Installing Python dependencies..."
-sudo pip install -r requirements.txt --quiet
+pip install -r requirements.txt --quiet
 
 echo "Gathering static files..."
-sudo ./manage.py collectstatic --noinput
+./manage.py collectstatic --noinput
 
 echo "Restarting Gunicorn..."
 sudo systemctl restart gunicorn
--- home/models.py
+++ home/models.py
@@ -4,8 +4,6 @@ from wagtail.core.models import Page
 
 class HomePage(Page):
 
-	parent_page_types = []
-
 	def get_context(self, request):
 		context = super(HomePage, self).get_context(request)
 		context['blog_posts'] = BlogPage.objects.live().public()
