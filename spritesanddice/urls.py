9834beebb5740d345b9d5751c65ae30069447497
juan0tron <glojonat@38f9d3e6d88b.ant.amazon.com>
2020-02-27 18:31:35 -0800

    Add Author to blog templates, small CSS fixes

--- spritesanddice/urls.py
+++ spritesanddice/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
 
 	url(r'^admin/snippets/', include(podcast_urls)),
 
-    url(r'^admin/', include(wagtailadmin_urls)),
+    url(r'^admin/',     include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
 
     url(r'^search/$', search_views.search, name='search'),
