19eeb672190b1534a939086b4ffaad1d1849147e
juan0tron <juandglover@gmail.com>
2020-04-12 18:36:35 -0700

    Add linux server deploy script

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+echo "Enabling Virtualenv..."
+source /home/sprites/sprites/bin/activate
+
+echo "Downloading updates from Git..."
+git checkout .
+git pull
+
+echo "Installing Python dependencies..."
+sudo pip install -r requirements.txt --quiet
+
+echo "Gathering static files..."
+sudo ./manage.py collectstatic --noinput
+
+echo "Restarting Gunicorn..."
+sudo systemctl restart gunicorn
+
+echo "Restarting nginx..."
+sudo nginx -t && sudo systemctl restart nginx
+
+echo "Done!"
