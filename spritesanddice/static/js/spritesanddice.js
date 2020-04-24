9dd01b68dae0fad76827d96a9bdc81732047f695
juan0tron <juandglover@gmail.com>
2020-04-24 11:55:16 -0700

    Move admin userbar from the navbar to the header

--- spritesanddice/static/js/spritesanddice.js
+++ spritesanddice/static/js/spritesanddice.js
@@ -1,6 +1,7 @@
 // Rip the .wagtail-userbar out of its default location and append it to the nav
 $(window).on('load', function(){
-	$('.wagtail-userbar').detach().appendTo('nav ul:last-child()');
+	$('.wagtail-userbar').detach().appendTo('header .userbar');
+	// $('.wagtail-userbar').detach().appendTo('nav ul:last-child()');
 })
 
 $(document).ready(function(){
