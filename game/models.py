b0223a1f7e8bd752ab11933d50cf1aa0a63d4494
juan0tron <juandglover@gmail.com>
2020-05-06 09:49:12 -0700

    Clean up Game model

--- game/models.py
+++ game/models.py
@@ -35,6 +35,14 @@ class GameTag(TaggedItemBase):
 
 @register_snippet
 class Game(index.Indexed, ClusterableModel):
+	title = models.CharField(max_length=255)
+
+	type = models.CharField(max_length=30, choices=(
+		('video-game',    'Video Game'),
+		('tabletop-game', 'Tabletop'),
+		('book',          'Book'),
+		('movie',         'Movie'),
+	), default='video-game')
 
 	box_art = models.ForeignKey(
 		'image.CustomImage',
@@ -44,24 +52,16 @@ class Game(index.Indexed, ClusterableModel):
 		related_name='+'
 	)
 
-	title = models.CharField(max_length=255)
-
-	type = models.CharField(max_length=30, choices=(
-		('video-game',    'Video Game'),
-		('tabletop-game', 'Tabletop'),
-		('book',          'Book'),
-	), default='video-game')
-
 	author    = models.CharField(max_length=255, blank=True)
 	designer  = models.CharField(max_length=255, blank=True)
 	developer = models.CharField(max_length=255, blank=True)
 	publisher = models.CharField(max_length=255, blank=True)
 
-	platforms = models.CharField(max_length=255, blank=True)
-	format    = models.CharField(max_length=255, blank=True)
+	platforms = models.CharField(max_length=255, blank=True, help_text="PC, PS4, Virtual Boy, Vectrex, etc.")
+	format    = models.CharField(max_length=255, blank=True, help_text="Card game, Miniatures game, Board game, etc.")
 
 	number_of_players = models.CharField(max_length=255, blank=True)
-	play_time = models.CharField(max_length=255, blank=True)
+	play_time = models.CharField(max_length=255, blank=True, help_text="Average play session duration for board games, total playtime for some video games.")
 
 	price        = models.CharField(max_length=255, blank=True)
 	release_date = models.DateField(blank=True, null=True)
@@ -75,29 +75,37 @@ class Game(index.Indexed, ClusterableModel):
 
 		ImageChooserPanel('box_art'),
 
-		FieldPanel('author', help_text="For books"),
-		FieldPanel('designer'),
-		FieldPanel('developer'),
-		FieldPanel('publisher'),
+		MultiFieldPanel([
+			FieldPanel('developer'),
+			FieldPanel('platforms'),
+		], heading="Video Game Specific Info", classname="collapsible"),
 
-		FieldPanel('platforms', help_text='PC, PS4, Virtual Boy, Vectrex, etc.'),
-		FieldPanel('format', help_text="Card game, Miniatures game, Board game, etc."),
-		FieldPanel('number_of_players'),
-		FieldPanel('play_time', help_text="Average play session duration for board games, total playtime for some video games."),
-		FieldPanel('price'),
-		FieldPanel('release_date'),
-		InlinePanel('other_info', heading="Other Info"),
+		MultiFieldPanel([
+			FieldPanel('designer'),
+			FieldPanel('author', help_text="For books"),
+			FieldPanel('format'),
+		], heading="Tabletop Specific Info", classname="collapsible"),
 
-		InlinePanel('review_codes', heading="Review codes"),
+		MultiFieldPanel([
+			FieldPanel('publisher'),
+			FieldPanel('number_of_players'),
+			FieldPanel('play_time'),
+			FieldPanel('price'),
+			FieldPanel('release_date'),
+		], heading="Other Game Info", classname="collapsible"),
+
+		InlinePanel('other_info', heading="Additional Notes"),
+
+		InlinePanel('review_copies', heading="Review Copies"),
 
 		FieldPanel('tags'),
 	]
 
-	def available_codes(self):
-		return self.review_codes.filter(redeemed=False).count()
+	def available_copies(self):
+		return self.review_copies.filter(redeemed_by=None).count()
 
-	def review_codes_(self):
-		return self.review_codes.count()
+	def review_copies_(self):
+		return self.review_copies.count()
 
 	def thumbnail(self):
 		if self.box_art:
@@ -125,18 +133,16 @@ class OtherInfo(Orderable):
 		FieldPanel('text'),
 	]
 
-class ReviewCodes(Orderable):
-	game = ParentalKey('Game', related_name='review_codes', on_delete=models.CASCADE)
+class ReviewCopy(Orderable):
+	game = ParentalKey('Game', related_name='review_copies', on_delete=models.CASCADE)
 
-	code = models.CharField(max_length=255, null=True, blank=True)
-	notes = models.TextField(blank=True)
-	redeemed = models.BooleanField(default=False)
-	redeemed_by = models.CharField(max_length=255, blank=True)
+	code        = models.CharField(max_length=255, null=True, blank=True, help_text="A download code, if it exists.")
+	notes       = models.TextField(blank=True, help_text="What version of the game? Is this DLC? An expansion? Who gave it to us?")
+	redeemed_by = models.CharField(max_length=255, blank=True, help_text="Who is reviewing this game?")
 
 	panels = [
 		FieldPanel('code'),
 		FieldPanel('notes'),
-		FieldPanel('redeemed'),
 		FieldPanel('redeemed_by'),
 	]
 
@@ -144,7 +150,7 @@ class ReviewCodes(Orderable):
 
 class GameAdmin(ModelAdmin):
 	model = Game
-	list_display = ('thumbnail', 'title', 'type', 'developer', 'publisher', 'review_codes_')
+	list_display = ('thumbnail', 'title', 'type', 'developer', 'publisher', 'review_copies_')
 	search_fields = [
 		'title',
 		'type',
