233c89d55ca66d4fec6beedd9b74a75a73f4908c
juan0tron <juandglover@gmail.com>
2020-05-08 15:45:36 -0700

    Bugfix for countup animation when summary items have a comma

--- spritesanddice/static/js/admin.js
+++ spritesanddice/static/js/admin.js
@@ -1,7 +1,7 @@
 function siteSummaryCountUp(){
 	$('.stats a span:not(.visuallyhidden)').each(function(){
 		let span = $(this);
-		$({ Counter: 0 }).animate({ Counter: $(span).text() }, {
+		$({ Counter: 0 }).animate({ Counter: $(span).text().replace(',','') }, {
 			duration: 1000,
 			easing: 'swing',
 			step: function (){
