0ccc8b9194b93b3addb507bc9fc9f2eefb57665d
juan0tron <juandglover@gmail.com>
2020-05-07 21:57:16 -0700

    forgot ALLOWED_HOSTS on prod settings

--- spritesanddice/settings/production.py
+++ spritesanddice/settings/production.py
@@ -1,9 +1,16 @@
 from .base import *
 from config import *
 
+INSTALLED_APPS += [
+]
+
 DEBUG = False
 
+ALLOWED_HOSTS = ['localhost', '64.227.25.145', 'spritesanddice.com']
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 try:
-	from .local import *
+    from .local import *
 except ImportError:
-	pass
+    pass
