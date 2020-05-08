375b972e1c29d431adba63cca4bed0ce7b9d6ca7
juan0tron <juandglover@gmail.com>
2020-05-07 20:36:26 -0700

    Open "redirects" link in redirect help text in a new tab

--- page/models.py
+++ page/models.py
@@ -186,7 +186,7 @@ class BlogPage(BasePage):
 			HelpPanel(content="""
 				<div style="max-width:650px;">
 					<p>Use this section to maintain any known URL paths this page has had in the past.</p>
-					<p>Once this page is published, the site will automatically create a <a href="/admin/redirects/">redirect</a> for you based on this list of legacy URLs if any changes you make cause this page's URL to change. This includes changing the "slug" field or moving the page to a new folder.</p>
+					<p>Once this page is published, the site will automatically create a <a href="/admin/redirects/" target="_blank">redirect</a> for you based on this list of legacy URLs if any changes you make cause this page's URL to change. This includes changing the "slug" field or moving the page to a new folder.</p>
 					<p>Note: If you edit or delete one of these legacy URLs, the corresponding redirect will be updated / deleted as well.</p>
 					<p>If this page has a comment thread, make sure to run the <a href="https://spritesanddice.disqus.com/admin/discussions/migrate/" target="_blank">Disqus Redirect Crawler</a> after verifying the redirect works so Disqus knows where this page has moved to.</p>
 				</div>
