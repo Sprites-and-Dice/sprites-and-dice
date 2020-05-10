ff82265ffbf85b09d277a2c86a11d1b50de066da
juan0tron <juandglover@gmail.com>
2020-05-09 23:26:04 -0700

    Fix conflicting og:image tags

--- home/models.py
+++ home/models.py
@@ -1,12 +1,24 @@
 from django.db import models
+
 from page.models import BlogPage
+
+from spritesanddice.models import SiteSettings
+
 from wagtail.core.models import Page
+
 
 class HomePage(Page):
 
 	parent_page_types = []
 
+	def header_image(self):
+		first_post = BlogPage.objects.live().public().order_by('-go_live_at').first()
+		if first_post.header_image:
+			return first_post.header_image
+		else:
+			return SiteSettings.objects.first().default_social_thumb
+
 	def get_context(self, request):
 		context = super(HomePage, self).get_context(request)
-		context['blog_posts'] = BlogPage.objects.live().order_by('-published')
+		context['blog_posts'] = BlogPage.objects.live().public().order_by('-go_live_at')
 		return context
